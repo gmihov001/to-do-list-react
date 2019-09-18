1308e024cfea83c2ad45430e49ec705cea2ee672
George Mihov <gmihov001@gmail.com>
2019-09-18 20:02:35 +0000

    Initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -8,8 +9,56 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+class ToDoMain extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			anInput: ""
+		};
+		this.updateState = this.updateState.bind(this);
+	}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+	updateState(e) {
+		this.setState({ anInput: e.target.value });
+	}
+
+	render() {
+		return (
+			<div className="container m-auto text-center">
+				<h1>Your To Do List</h1>
+				<input
+					type="text"
+					value={this.state.anInput}
+					onChange={this.updateState}
+					placeholder="Enter activity here"
+				/>
+				<button
+					type="button"
+					className="btn"
+					onClick={() => {
+						let ul = document.querySelector("#toDoList");
+						ul.innerHTML += `<li>${this.state.anInput}</li>`;
+					}}>
+					Enter Activity
+				</button>
+				<br />
+				{this.state.anInput}
+				<br />
+				<ul id="toDoList">
+					<li>Example</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<ToDoMain />, document.querySelector("#app"));
+
+/* class DeleteEntry extends React.Component {
+    render() {
+        return (
+
+        );
+    }
+}
+*/
